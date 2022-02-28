ffa2f6899309e0228bde7fad53da994c49618ea4
jibundeyare <contact@jibundeyare.com>
2022-02-28 16:03:13 +0100

    Création du repo
    Ajout des premiers chapitre de cours.
    Ajout des exo correspondants.

---
+++ exercice-01-hello.py
@@ -0,0 +1,13 @@
+# exercice-01-hello.py
+
+# exo 1.1
+# Affichez le message "programme démarré" en utilisant des doubles quotes
+
+# réponse 1.1
+foo bar baz
+
+# exo 1.2
+# Affichez le message "programme terminé" en utilisant des simples quotes
+
+# réponse 1.2
+
---
+++ variables.py
@@ -0,0 +1,41 @@
+# ceci est un commentaire
+
+nombre = -123
+print(nombre)
+
+nombre = -123.0
+print(nombre)
+
+nombre = -3.14
+print(nombre)
+
+nombre = "-123"
+print(nombre)
+
+text = "lorem ipsum"
+print(text)
+
+is_day = True
+print(is_day)
+
+has_sugar = False
+print(has_sugar)
+
+has_accepted_ula = None
+print(has_accepted_ula)
+
+html_code = '<h1>Titre de mon blog</h1>'
+print(html_code)
+
+nickname = "John \"Dead\" Doe"
+nickname = 'John O\'Connor'
+print(nickname)
+
+multiline_text = "\\foo\nbar\nbaz"
+print(multiline_text)
+
+multiline_text = """'foo'
+"bar"
+baz"""
+print(multiline_text)
+
