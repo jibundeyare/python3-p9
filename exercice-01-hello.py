acfbee44378c531926b0f4d197c2874037dd06c0
jibundeyare <contact@jibundeyare.com>
2022-02-28 16:49:06 +0100

    Fin du premier exo
    C'était facile.

--- exercice-01-hello.py
+++ exercice-01-hello.py
@@ -4,10 +4,10 @@
 # Affichez le message "programme démarré" en utilisant des doubles quotes
 
 # réponse 1.1
-foo bar baz
+print("programme démarré")
 
 # exo 1.2
 # Affichez le message "programme terminé" en utilisant des simples quotes
 
 # réponse 1.2
-
+print("programme terminé")
