961772cc8aa89795558c3cc290b1e8ec0470f9eb
jibundeyare <contact@jibundeyare.com>
2022-03-02 11:58:28 +0100

    Mise à jour cours variables
    Exploration de conversions de type.

--- variables.py
+++ variables.py
@@ -2,27 +2,33 @@
 
 nombre = -123
 print(nombre)
+print(type(nombre))
 
 nombre = -123.0
 print(nombre)
+print(type(nombre))
 
 nombre = -3.14
 print(nombre)
+print(type(nombre))
 
 nombre = "-123"
 print(nombre)
+print(type(nombre))
 
 text = "lorem ipsum"
 print(text)
 
 is_day = True
 print(is_day)
+print(type(is_day))
 
 has_sugar = False
 print(has_sugar)
 
 has_accepted_ula = None
 print(has_accepted_ula)
+print(type(has_accepted_ula))
 
 html_code = '<h1>Titre de mon blog</h1>'
 print(html_code)
@@ -39,3 +45,99 @@ multiline_text = """'foo'
 baz"""
 print(multiline_text)
 
+nombre = "5"
+print(nombre)
+print(type(nombre))
+nombre = int(nombre)
+print(nombre)
+print(type(nombre))
+
+nombre = "2.71"
+print(nombre)
+print(type(nombre))
+nombre = float(nombre)
+print(nombre)
+print(type(nombre))
+
+nombre = 3.14
+print(nombre)
+print(type(nombre))
+nombre = int(nombre)
+print(nombre)
+print(type(nombre))
+
+texte = str(nombre)
+print(texte)
+print(type(texte))
+
+my_var = 0
+my_var = bool(my_var)
+print(my_var)
+
+my_var = 1
+my_var = bool(my_var)
+print(my_var)
+
+my_var = 123
+my_var = bool(my_var)
+print(my_var)
+
+my_var = -1
+my_var = bool(my_var)
+print(my_var)
+
+my_var = -123
+my_var = bool(my_var)
+print(my_var)
+
+my_var = 0.0
+my_var = bool(my_var)
+print(my_var)
+
+my_var = 0.000001
+my_var = bool(my_var)
+print(my_var)
+
+my_var = ""
+my_var = bool(my_var)
+print(my_var)
+
+my_var = " "
+my_var = bool(my_var)
+print(my_var)
+
+my_var = "0"
+my_var = bool(my_var)
+print(my_var)
+
+my_var = "123"
+my_var = bool(my_var)
+print(my_var)
+
+my_var = "-123"
+my_var = bool(my_var)
+print(my_var)
+
+my_var = [123, "abc", False]
+my_var = bool(my_var)
+print(my_var)
+
+my_var = [False]
+my_var = bool(my_var)
+print(my_var)
+
+my_var = []
+my_var = bool(my_var)
+print(my_var)
+
+my_var = [None]
+my_var = bool(my_var)
+print(my_var)
+
+my_var = [[]]
+my_var = bool(my_var)
+print(my_var)
+
+my_var = None
+my_var = bool(my_var)
+print(my_var)
