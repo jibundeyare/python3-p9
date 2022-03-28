75079a47b5585a7314ee68cf39f460d59280a130
jibundeyare <contact@jibundeyare.com>
2022-03-28 11:52:18 +0200

    Ajout d'un cours sur la complexité

---
+++ complexite.py
@@ -0,0 +1,68 @@
+# complexité == efficacité
+# efficacité :
+# - temps d'exécution
+# - espace mémoire
+
+# notation de Landau : O() (lettre O, pas le chiffre 0)
+# pas une fonction mais un ensemble
+
+# O(c) (c == constante)
+# O(1)
+result = 123 + 42
+print(result)
+
+# O(n)
+# n est la quantité de données à traiter
+numbers = [123, 42, 3.14]
+
+for number in numbers:
+    result = number * 2
+    print(result)
+
+# O(n * m) == O(n * n) == O(n²)
+# n est la quantité de données à traiter de la première liste
+# m est la quantité de données à traiter de la deuxième liste
+numbers = [123, 42, 3.14]
+more_numbers = [2.71, 3.14, 0, 53]
+common_numbers = []
+
+for number in numbers:
+    for more_number in more_numbers:
+        if number == more_number:
+            common_numbers.append(number)
+
+# O(n * n) == O(n²)
+matrix = [
+    [123, 42, 3.14],
+    [2.71, 3.14, 0],
+    [1, 2, 3]
+]
+
+for line in matrix:
+    for number in line:
+        result = number * 2
+        print(result)
+
+# O(n * n * n) == O(n³)
+cube = [
+    [
+        ['a1', 'a2', 'a3'],
+        ['a4', 'a5', 'a6'],
+        ['a7', 'a8', 'a9'],
+    ],
+    [
+        ['b1', 'b2', 'b3'],
+        ['b4', 'b5', 'b6'],
+        ['b7', 'b8', 'b9'],
+    ],
+    [
+        ['c1', 'c2', 'c3'],
+        ['c4', 'c5', 'c6'],
+        ['c7', 'c8', 'c9'],
+    ]
+]
+
+for square in cube:
+    for line in square:
+        for spot in line:
+            print(spot)
