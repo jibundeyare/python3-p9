79ba2bcb3a2b5720182f17f393ec8f377decb89c
jibundeyare <contact@jibundeyare.com>
2022-03-28 15:01:33 +0200

    Exos de révisions

---
+++ revisiting.py
@@ -0,0 +1,47 @@
+import random
+
+# Exo 1.1
+# Affichez le type de donnée des variables a et b.
+
+a = 3.14
+b = 3
+
+# Réponse 1.1
+
+# Exo 1.2
+# En utilisant les variables a et b, affichez les chiffres après la virgule de la variable a.
+
+a = 3.14
+b = 3
+
+# Réponse 1.2
+
+# Exo 1.3
+# Affichez le type de données de la variable n.
+
+n = random.randint(10, 99) / 10
+
+# Réponse 1.3
+
+# Exo 1.4
+# Convertissez n en un nombre entier, stockez le résultat dans une variable et affichez le résultat.
+
+n = random.randint(10, 99) / 10
+
+# Réponse 1.4
+
+# Exo 1.5
+# Affichez :
+# - les nombres avant la virgule de la variable n
+# - les nombres après la virgule de la variable n
+
+n = random.randint(10, 99) / 10
+
+# Réponse 1.5
+
+# Exo 1.5
+# Stockez dans une variable si la variable n est un nombre entier ou non.
+
+n = random.randint(10, 99) / 10
+
+# Réponse 1.5
