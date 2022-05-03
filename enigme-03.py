ab1b67ed199d04a57955b31a5141b1737d97fa0b
jibundeyare <contact@jibundeyare.com>
2022-05-03 10:53:48 +0200

    Ajout d'une énigme

---
+++ enigme-03.py
@@ -0,0 +1,2 @@
+# Un artilleur dispose de boulets de canon répartis dans un carré parfait. Pour réduire l'encombrement au sol, l'artilleur réussit à empiler ses boulets pour former une belle pyramide à base carrée.
+# Quel est le plus petit nombre de boulets possible dont dispose l'artilleur ?
