ad5263d4a6ee2becb2a37d47d822b00e71e7619c
jibundeyare <contact@jibundeyare.com>
2022-05-03 11:32:27 +0200

    Améliorations

--- enigme-00.py
+++ enigme-00.py
@@ -3,7 +3,9 @@ from timeit import default_timer as timer
 # start benchmark
 start = timer()
 
-# votre code ici
+for _ in range(0, 100000):
+    # votre code ici
+    pass
 
 # stop benchmark
 end = timer()
